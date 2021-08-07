4d3e48bf7871e1ca1394a2cc4ef4b84fdd0e2e65
Prathamesh Polsane <88595588+PolsanePrathamesh@users.noreply.github.com>
2021-08-08 01:26:59 +0530

    Add files via upload
    first commit.

---
+++ Keys.js
@@ -0,0 +1,4 @@
+//API KEY
+export const API_KEY = "AIzaSyAXN1BDnbwe_bN4cv9rLZJJ6jFvae7xnF8";
+//Context KEY
+export const CONTEXT_KEY = "59a9305b69fab7bcd";
---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  future: {
+    webpack5: true,
+  },
+  images: {
+    domains: [
+      "www.google.co.in",
+      "https://yt3.ggpht.com/ytc/AAUvwng6FVquOzMBPsBxWDwJCNiaCZENiaPNDA_2DXJptA=s900-c-k-c0x00ffffff-no-rj",
+    ],
+  },
+};
